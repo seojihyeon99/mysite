4ffff423daa43e723b6e46e5c3682788d7aa8ce3
seojihyeon99 <seojihyeon99@naver.com>
2024-12-18 15:51:41 +0900

    241218 mysite 과제 (회원가입)

---
+++ mysite02/src/main/java/mysite/controller/MainServlet.java
@@ -0,0 +1,24 @@
+package mysite.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet({"", "/main"}) // '/'는 특별한 의미 => 모든 요청(css, image 등)을 처리한다. => 따라서 '/' 대신 "" 써야!!
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");
+		rd.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
