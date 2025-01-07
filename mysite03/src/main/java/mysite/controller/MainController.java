ac714e7a44a6dcad746f334122900f9356e03cae
seojihyeon99 <seojihyeon99@naver.com>
2025-01-07 18:13:01 +0900

    250103 mysite Admin MultipartResolver (File Upload) 적용

--- mysite03/src/main/java/mysite/controller/MainController.java
+++ mysite03/src/main/java/mysite/controller/MainController.java
@@ -1,14 +1,21 @@
 package mysite.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import mysite.service.SiteService;
 
 @Controller
 public class MainController {
+	private final SiteService siteService;
 	
+	public MainController(SiteService siteService) {
+		this.siteService = siteService;
+	}
+
 	@RequestMapping({"/", "/main"})
-	public String index() {
+	public String main(Model model) {
+		model.addAttribute("siteVo", siteService.getSite());
 		return "main/index";
 	}
 }
